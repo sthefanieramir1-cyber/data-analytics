165093fa9247de24beb08b483426f9bd2def08fc
Sthefanie Hernandez <sramirez@my.yearupunited.org>
2026-04-15 14:53:02 -0700

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,7 @@
 /*
 Shakila is a database used to collect information about A DVD rental store and for use by students and developers to practice SQL
  */
+USE sakila;
+SELECT * FROM actor; -- Retrieved [200] records
+
+SELECT * FROM film; -- Retrieved [1000] records
