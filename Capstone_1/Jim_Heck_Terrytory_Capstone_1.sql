055498770fa9ff3b6fecde9c14bac20860296e73
Sthefanie Hernandez <sramirez@my.yearupunited.org>
2026-04-28 00:55:23 -0700

    Coloreado Revenue

---
+++ Capstone_1/Jim_Heck_Terrytory_Capstone_1.sql
@@ -0,0 +1,24 @@
+-- What is total revenue overall for sales in the assigned territory, plus the start date and end date
+-- that tell you what period the data covers?
+
+
+
+
+
+
+-- What is the month by month revenue breakdown for the sales territory?
+
+
+
+--  Provide a comparison of total revenue for the specific sales territory and the region it belongs to.
+--  What is the number of transactions per month and average transaction size by product category
+-- for the sales territory?
+
+
+
+-- Can you provide a ranking of in-store sales performance by each store in the sales territory, or a
+-- ranking of online sales performance by state within an online sales territory?
+
+
+
+-- What is your recommendation for where to focus sales attention in the next quarter?
