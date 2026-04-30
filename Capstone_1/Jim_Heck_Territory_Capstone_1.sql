332e775541949b069cbbd54325fc595159bed74f
Sthefanie Hernandez <sramirez@my.yearupunited.org>
2026-04-30 15:15:31 -0700

    final parts of captone project

--- Capstone_1/Jim_Heck_Territory_Capstone_1.sql
+++ Capstone_1/Jim_Heck_Territory_Capstone_1.sql
@@ -3,96 +3,137 @@
 -- location
 
 USE sample_sales;
-
 SELECT
-lo.State,
-MIN(Transaction_Date) AS Start_Date, 
-MAX(Transaction_Date) AS End_Date,
-SUM(Sale_Amount) AS Revenue
-FROM store_locations AS lo
+	sl.State,
+	MIN(Transaction_Date) AS Start_Date, 
+	MAX(Transaction_Date) AS End_Date,
+	SUM(Sale_Amount) AS Revenue
+FROM 
+store_locations AS sl
 INNER JOIN store_sales AS sa
-ON lo.StoreId = sa.Store_ID
+	ON sa.Store_ID = sl.StoreId
 WHERE State = 'Colorado'
 GROUP BY State;
 
 -- What is the month by month revenue breakdown for the sales territory?
 
 SELECT
-DATE_FORMAT(Transaction_Date, '%Y-%m') AS Monthly_Sales22,
-SUM(Sale_Amount) AS Revenue
-FROM store_locations AS lo
+	DATE_FORMAT(Transaction_Date, '%Y-%m') AS Monthly_Sales_Co,
+	SUM(Sale_Amount) AS Revenue
+FROM  store_locations AS sl
 INNER JOIN store_sales AS sa
-ON lo.StoreId = sa.Store_ID
-WHERE State = 'Colorado'
-GROUP BY Monthly_Sales22;
+	ON sa.Store_ID = sl.StoreId 
+
+WHERE  State = 'Colorado'
+GROUP BY Monthly_Sales_Co
+ORDER BY Monthly_Sales_Co;
 
 
 --  Provide a comparison of total revenue for the specific sales territory and the region it belongs to.
 
 -- Colorado Physical Store territory Revenue
-WITH ColoradoPh_Store AS
+WITH CO_Physical_Store_Revenue AS 
 ( 
-SELECT
-lo.State as State,
-MIN(sa.Transaction_Date) AS Start_Date, 
-MAX(sa.Transaction_Date) AS End_Date,
-SUM(sa.Sale_Amount) AS Revenue,
-mg.Region
-FROM store_locations AS lo
+SELECT 
+	sl.State AS State,
+    MIN(sa.Transaction_Date) AS Start_Date, 
+    MAX(sa.Transaction_Date) AS End_Date,
+	SUM(sa.Sale_Amount) AS Revenue,
+	mg.Region
+FROM store_locations AS sl
 INNER JOIN store_sales AS sa
-ON lo.StoreId = sa.Store_ID
+	ON sa.Store_ID = sl.StoreId
 INNER JOIN management AS mg
-ON lo.State = mg.State
-WHERE lo.State = 'Colorado'
-GROUP BY lo.State, mg.Region
-),
+	ON mg.State = sl.State
+WHERE sl.State = 'Colorado'
+GROUP BY sl.State, mg.Region),
+
 -- Online West Region Revenue
 WestR_Online_Store AS
 (
 SELECT 
-mg.State AS State,
-MIN(os.Date) AS Start_Date, 
-MAX(os.Date) AS End_Date,
-SUM(os.SalesTotal) AS Revenue_By_ST,
-mg.Region
+	mg.State AS State,
+    MAX(os.Date) AS End_Date,
+	MIN(os.Date) AS Start_Date, 
+	SUM(os.SalesTotal) AS Revenue_By_ST,
+	mg.Region
 FROM online_sales AS os
 INNER JOIN management AS mg
-ON mg.State = 'Online Sales'
+	ON mg.State = 'Online Sales'
 GROUP BY mg.State, mg.Region
 )
 
-SELECT * FROM ColoradoPh_Store 
+SELECT * FROM CO_Physical_Store_Revenue 
 UNION ALL 
 SELECT * FROM WestR_Online_Store;
 
 --  What is the number of transactions per month and average transaction size by product category
 -- for the sales territory?
 
--- Sales per month and average transactions size in Colorado physical store
+-- Sales per month and average transactions size in Colorado physical stores
+WITH Colorado_Sales AS
+(  
 SELECT
-sa.Store_ID,  
-DATE_FORMAT(sa.Transaction_Date, '%Y-%m') AS Monthly_Sales_CL,
-pr.Categoryid AS Products_Category,
-sa.Prod_Num,
-AVG(sa.Sale_Amount) AS Revenue
+	sa.Store_ID,
+	DATE_FORMAT(sa.Transaction_Date, '%Y-%m') AS Monthly_Sales,
+	iv.Category AS Products_Category,
+	sa.Sale_Amount 
 FROM Store_sales AS sa
 INNER JOIN Products AS pr
-ON pr.ProdNum = sa.Prod_Num
-INNER JOIN store_locations AS lo
-ON lo.StoreId = sa.Store_ID
-WHERE lo.State = 'Colorado'
-GROUP BY
-sa.Store_ID,
-Monthly_Sales_CL,
-pr.Categoryid,
-sa.Prod_Num;
+ ON pr.ProdNum = sa.Prod_Num
+INNER JOIN store_locations AS sl
+ ON sl.StoreId = sa.Store_ID
+ INNER JOIN inventory_categories AS iv
+	ON iv.Categoryid = pr.Categoryid
+WHERE sl.State = 'Colorado'
+),
 
-
-
+Revenue_By_ProdcutsCO AS 
+(
+SELECT 
+	Monthly_Sales,
+	COUNT(Products_Category) AS Total_Transactions,
+    SUM(Sale_Amount) AS Total_Revenue, 
+    ROUND(AVG(Sale_Amount), 4) AS Average_Revenue,
+SUM(CASE WHEN Products_Category = 'Apparel and Merchandise' THEN Sale_Amount ELSE 0 END) AS Apparel,
+SUM(CASE WHEN Products_Category = 'Art Supplies' THEN Sale_Amount ELSE 0 END) AS Art_Supplies,
+SUM(CASE WHEN Products_Category = 'Books (General)' THEN Sale_Amount ELSE 0 END) AS Books,
+SUM(CASE WHEN Products_Category = 'Stationery and Supplies' THEN Sale_Amount ELSE 0 END) AS Stationery,
+SUM(CASE WHEN Products_Category = 'Technology & Accessories' THEN Sale_Amount ELSE 0 END) AS Technology,
+SUM(CASE WHEN Products_Category = 'Textbooks' THEN Sale_Amount ELSE 0 END) AS Textbooks
+FROM Colorado_Sales
+GROUP BY Monthly_Sales
+)
+SELECT * FROM Revenue_By_ProdcutsCO
+ORDER BY Monthly_Sales;
 
 -- Can you provide a ranking of in-store sales performance by each store in the sales territory, or a
 -- ranking of online sales performance by state within an online sales territory?
 
-
-
+-- Subquery 
+    
+SELECT
+    Store_Location,
+    Monthly_Sales,
+    Total_Transactions,
+    Total_Revenue,
+    Average_Revenue,
+    RANK() OVER (ORDER BY Total_Revenue DESC) AS Store_Rank
+FROM (
+  SELECT
+	sa.Store_ID,
+    sl.StoreLocation AS Store_Location,
+	DATE_FORMAT(sa.Transaction_Date, '%Y-%m') AS Monthly_Sales,
+	COUNT(sa.Sale_Amount) AS Total_Transactions,
+	SUM(sa.Sale_Amount) AS Total_Revenue,
+	ROUND(AVG(sa.Sale_Amount), 4) AS Average_Revenue
+FROM Store_sales AS sa
+INNER JOIN Products AS pr
+ ON pr.ProdNum = sa.Prod_Num
+INNER JOIN store_locations AS sl
+ ON sl.StoreId = sa.Store_ID
+WHERE sl.State = 'Colorado'
+GROUP BY sa.Store_ID, sl.StoreLocation, Monthly_Sales
+) AS CO_Store_Ranking
+ORDER BY Store_Rank, Monthly_Sales;
 -- What is your recommendation for where to focus sales attention in the next quarter?
