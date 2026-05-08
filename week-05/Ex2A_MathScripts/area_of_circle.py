96bef7ecf354648bb4c357d46d78355056827249
Sthefanie Hernandez <sramirez@my.yearupunited.org>
2026-05-07 22:32:51 -0700

    area of a circle found

---
+++ week-05/Ex2A_MathScripts/area_of_circle.py
@@ -0,0 +1,12 @@
+# Birthday date 18 as diameter
+
+import math
+#
+diameter = 18 
+radius = diameter / 2
+
+# calculate area of circle
+
+area = math.pi * radius ** 2
+
+print("The area of the circle with diameter", diameter, "is:", area)
