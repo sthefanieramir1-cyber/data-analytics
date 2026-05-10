6fe5934579c8fcde44b816800fa552bc932898a0
Sthefanie Hernandez <sramirez@my.yearupunited.org>
2026-05-10 12:08:07 -0700

    c commit

---
+++ week-05/Ex3A_conversion_tests.py
@@ -0,0 +1,13 @@
+# Description: This script tests various numeric
+# conversion techniques
+# Author: Sam Q. Newprogrammer
+
+a = " 101.1 " # with spaces around the number  
+b = '55' #clean number as a string
+c = "402 Stevens" # number with text after it
+d = 'Number 5  ' # number with text before it and spaces after
+
+
+# Cast as integer using int()
+
+# Cast as float using float()
