7a34e904e13f1637ebb8a6d1edd6e083e7c9b18b
Sthefanie RH <sthefanieramir1@gmail.comm>
2026-05-10 14:59:43 -0700

    5w- movies exercise

---
+++ week-05/Ex3B_SampleCollections/movie_list.py
@@ -0,0 +1,21 @@
+# List of movies that I’d like to watch
+sthef_movies = ['The Matrix', 'Interstellar', 'Zootopia 2']
+
+print(f'The list sthef_movies includes the {len(sthef_movies)} movies I’d like to watch')
+
+print(sthef_movies)
+
+# Sort the list in alphabetical order
+print(f'Sorted list: {sorted(sthef_movies)}')
+
+print(f'Original list: {sthef_movies}') # the original list is unchanged
+
+# The comparison of two lists is based on the order of the elements in the list,
+#  not on the content of the list.
+
+# Add a new movies to the list
+
+sthef_movies.append('The Lord of the Rings, The Fellowship of the Ring')
+
+print(f'Updated list: {sorted(sthef_movies)}')
+
